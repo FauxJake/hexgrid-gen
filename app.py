6a66dff5eea1440bba96fac0cd79ec1f2460aa35
Jake Riesser <jake.riesser@gmail.com>
2022-03-10 15:11:12 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import random
+
+def main():
+    # grid is 13 x 11
+    grid = [[0 for i in range(13)] for j in range(11)]
+    
+    easyEncounters = 0
+    mediumEncounters = 0
+    hardEncounters = 0
+    deadlyEncounters = 0
+    safeZones = 0
+    
+
+
+if __name__ == '__main__':
+    # todo get args from cmd line
+    main()
